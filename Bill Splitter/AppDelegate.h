a05cd92034e6344675da78da6c497539a2c43b22
Chloe Horgan <super.chloe@gmail.com>
2016-01-24 12:25:05 -0500

    Initial Commit

---
+++ Bill Splitter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Bill Splitter
+//
+//  Created by Chloe on 2016-01-24.
+//  Copyright © 2016 Chloe Horgan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Bill Splitter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Bill Splitter
+//
+//  Created by Chloe on 2016-01-24.
+//  Copyright © 2016 Chloe Horgan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
